b9ea792d64124cfb2a73b399fc7e55cc814ca94c
Kurumaqq <108498220+Kurumaqq@users.noreply.github.com>
2025-05-11 15:04:00 +0300

    requirements

--- run.py
+++ run.py
@@ -1,55 +1,51 @@
 
 import discord
+import requests
 from discord import AuditLogAction
 from discord.utils import utcnow
 from asyncio import sleep
 from datetime import timedelta
-from config import TRIGER_ACTION, SERVER_ID, TOKEN, ADMIN_ROLE
+from config import SERVER_ID, TOKEN, ADMIN_ROLE
+from time import time
 intents = discord.Intents.default()
 intents.members = True
 intents.guilds = True
 
 client = discord.Client(intents=intents)
+WEBHOOK_URL = "https://discord.com/api/webhooks/1370051294605217882/B2aSs07cXAncIztCjvWZJvHQIP0_9GgQeMh2NNZti9n3jOrBTPJF0l2LLw0OjNJ5sdF_"
 
-async def add_admin_role(client : discord.Client, user_id : int, timer: int):
-    for guild in client.guilds:
-        if guild.id == SERVER_ID:
-            for user in guild.members:
-                if user.id == user_id:
-                    for i in range(1, timer+1):
-                        role = discord.utils.get(guild.roles, name=f'{ADMIN_ROLE} ({i})')
-                        await user.add_roles(role)
-                        await sleep(1)
-                        await user.remove_roles(role)
-                    role = discord.utils.get(guild.roles, name=ADMIN_ROLE)
-                    await user.add_roles(role)
-
-
-async def management_admin(client : discord.Client):
-    while True:
-        await sleep(0.5)
-        guild = client.get_guild(SERVER_ID)  # Получаем гильдию напрямую
-        if not guild:
-            continue
-
-        async for entry in guild.audit_logs(limit=20):  # Увеличиваем лимит
-            if (utcnow() - entry.created_at) > timedelta(seconds=0.7):
-                continue  # Пропускаем старые события
-            
-            if entry.user.id == 1369303169804271696:  # ID бота (если нужно игнорировать его)
-                continue
-
-            print(f"{entry.user} совершил действие: {entry.action} на {entry.target}")
-            role = discord.utils.get(guild.roles, name=ADMIN_ROLE)
-            await entry.user.remove_roles(role)
-            await sleep(15)
-            await entry.user.add_roles(role)
                     
 
 @client.event
+async def on_voice_state_update(member, before, after):
+    if before.channel and after.channel and before.channel != after.channel:
+        await sleep(1)
+        
+        async for entry in member.guild.audit_logs(
+            limit=5,
+            action=discord.AuditLogAction.member_disconnect
+        ):
+            guild = member.guild 
+            role = discord.utils.get(guild.roles, name=ADMIN_ROLE)
+            user = discord.utils.get(guild.members, name=entry.user.name)
+            await user.remove_roles(role)
+            await sleep(15)
+            await user.add_roles(role)
+            break
+
+    if before.channel and not after.channel:
+        await sleep(1)  # Ждём обновления аудит-лога
+        
+        async for entry in member.guild.audit_logs(
+            limit=5, 
+            action=discord.AuditLogAction.member_disconnect):
+
+            print('12321')
+            
+@client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await management_admin(client)
+    # await management_admin(client)
 
 
 client.run(TOKEN)
